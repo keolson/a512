e65dd0323055dc9a607dd956fdde33fca6039004
keolson <kevin@sharethis.com>
2009-07-12 16:02:19 -0700

    pull live data from pacos api

--- js/core.js
+++ js/core.js
@@ -1,31 +1,77 @@
+function genTopicCloud() {
+    // Make an array of sample tags
+    var sampleData = {
+        results : [
+            {tag: 'Social Security', count: 5},
+            {tag: 'Retirement', count: 9},
+            {tag: 'Savings Account', count: 5},
+            {tag: 'Stock Trading', count: 7},
+            {tag: 'Investment', count: 7},
+        ]};
+	
+	sampleData.results.sort( function(){return (Math.round(Math.random())-0.5)});
+    
+          /* 
+           * Here we use a MemoryProxy to read in the JSON data.
+           * 
+           * This store could potentially read from any source you'd like (in-memory
+           * or stored server-side), and a variety of data formats from JSON, XML, 
+           * or simple arrays using the variety of DataProxy and DataReader 
+           * implementations supplied in Ext.data.
+           */
+    var ds = new Ext.data.Store({
+        proxy: new Ext.data.MemoryProxy(sampleData),
+        reader: new Ext.data.JsonReader({
+            root: 'results',
+            
+              }, [
+                  {name: 'tag'},
+      			  {name: 'count'}
+              ]),
+        remoteSort: false
+    });
+	
+    var cloud = new Ext.ux.TagCloud({
+        store: ds, 
+        displayField: 'tag', 
+        weightField: 'count', 
+        displayWeight: false
+    }
+      							   );
+	
+    cloud.on('tagselect', function(cloud, record, index){
+        alert('You clicked on "'+record.get('tag')+'"!');
+    });
+    cloud.render('cloud');
+	
+    ds.load();
+}
 
-Ext.onReady(function(){
-	//Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
-
+function genContentList() {
 	var store = new Ext.ux.data.PagingStore({
 		totalProperty: 'count',
 		root: 'items',
 		idProperty: 'num',
 		fields: ['title', 'views', 'shares', 'services'],
 		proxy: new Ext.data.HttpProxy({
-			url: '../api/getResults.php',
+			url: '../api/getTopics.php',
 		}),
 		reader: new Ext.data.JsonReader({
-			totalProperty: 'count',
-			root: 'items',
+			totalProperty: 'url_count',
+			root: 'urls',
 		}, [
 			{name: 'title'},
 			{name: 'views'},
 			{name: 'shares'},
 			{name: 'services'}
 		]),
-		autoLoad: {params: {start: 0, limit: 4}}
+		autoLoad: { params: { start: 0, limit: 4, domain: 'foxnews.com', period: 30, topic: 'Obama' } }
 	});
 
 	var tpl = new Ext.XTemplate( 
 		'<tpl for=".">',
 		'<div class="listElement">',
-		    '<div class="resultTitle">{title}</div>',
+		    '<div class="resultTitle">{[Ext.util.Format.ellipsis(values.title, 60, true)]}</div>',
 		    '<div class="resultViews">{views}</div>',
 		    '<div class="resultShares">{shares}</div>',
 		    '<div class="resultServices">{[this.formatServices(values.services)]}</div>',
@@ -64,6 +110,13 @@ Ext.onReady(function(){
 	panel.render('content');
 
 	store.load();
+}
+
+
+Ext.onReady(function(){
+	//Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+	genTopicCloud();
+	genContentList();
 
 });
 						
