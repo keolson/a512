b2dd6bb956839701b748a488269a3eaf1793c3bb
keolson <kevin@sharethis.com>
2009-07-13 11:08:43 -0700

    convert pacos bitmask to service list

--- api/getTopics.php
+++ api/getTopics.php
@@ -23,13 +23,52 @@ try {
 	$info = curl_getinfo($curl);
 	curl_close($curl);
 
-	echo $return_str;
+	$results =  json_decode($return_str,true);
 
+	foreach ( $results['urls'] as $key => $url ) {
+		$results['urls'][$key]['services'] = convertServices( $url['dest_bits'] );
+	}
+
+	print_r($results['urls']);
 }
 catch( Exception $e ) {
 	throw $e;
 }
 
+function convertServices($bitstr) {
+	$dests = array (
+					'email' => 0,
+					'facebook' => 1,
+					'twitter' => 2,
+					'myspace' => 3,
+					'digg' => 4,
+					'live' => 5,
+					'delicious' => 6,
+					'reddit' => 7,
+					'stumbleupon' => 8,
+					'google' => 9,
+					'sms' => 10,
+					'linkedin' => 11,
+					'buzz' => 12,
+					'yahoo' => 13,
+					);
+
+	$services = "";
+	foreach( $dests as $key => $value ){
+		$bit_pos = $value;
+		$mask = 1 << $bit_pos;
+		if( $bitstr & $mask ){
+			if( $services == "" ){
+				$services = $key;
+			} else {
+				$services .= ',' . $key;
+			}
+			
+		}
+	}
+	
+	return $services;
+}
 
 
 
